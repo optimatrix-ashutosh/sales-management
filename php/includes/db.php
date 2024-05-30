5874e0ba080ca429542a06ade9bc9a6c86a4b727
Ashutosh Dave <157338914+optimatrix-ashutosh@users.noreply.github.com>
2024-05-30 17:18:44 +0530

    Update db.php

--- php/includes/db.php
+++ php/includes/db.php
@@ -1,1 +1,12 @@
+<?php
 
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+    $dbname = "product_sales";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
