b439c59132a42d3f2e95ee8aa026af7f6af33eb5
Ashutosh Dave <157338914+optimatrix-ashutosh@users.noreply.github.com>
2024-05-30 17:19:53 +0530

    Update auth.php

--- php/includes/auth.php
+++ php/includes/auth.php
@@ -1,1 +1,30 @@
+<?php
+session_start();
+
+function isAuthenticated() {
+    return isset($_SESSION['userId']);
+}
+
+function login($username, $password, $conn) {
+    $stmt = $conn->prepare("SELECT id, password FROM Users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $hashed_password);
+        $stmt->fetch();
+        if (password_verify($password, $hashed_password)) {
+            $_SESSION['userId'] = $id;
+            return true;
+        }
+    }
+    return false;
+}
+
+function logout() {
+    session_destroy();
+    header("Location: ../views/login.html");
+    exit;
+}
+
 
